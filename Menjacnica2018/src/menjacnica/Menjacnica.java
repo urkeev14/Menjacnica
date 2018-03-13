31c74d3b4254601528d0764aea03c731de49cffc
urkeev14 <uros.veljkovic1996@gmail.com>
2018-03-13 22:40:24 +0100

    Izmene metode dodajKurs u klasi Menjacnica

--- Menjacnica2018/src/menjacnica/Menjacnica.java
+++ Menjacnica2018/src/menjacnica/Menjacnica.java
@@ -1,27 +1,35 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import interfjes.InterfejsMenjacnice;
 import valuta.Valuta;
 
-public class Menjacnica implements InterfejsMenjacnice{
+public class Menjacnica implements InterfejsMenjacnice {
+
+	LinkedList<Valuta> valuteMenjacnice = new LinkedList<>();
 
 	@Override
-	public void dodajKurs(Valuta valuta, GregorianCalendar dan) {
-		
+	public void dodajKurs(Valuta valuta) {
+		valuteMenjacnice.add(valuta);
 	}
 
 	@Override
-	public void obrisiKurs(Valuta valuta, GregorianCalendar dan) {
-		
+	public void obrisiKurs(Valuta valuta) {
+		valuteMenjacnice.remove(valuta);
 	}
 
 	@Override
-	public Valuta pronadjiValutu(Valuta valuta, GregorianCalendar dan) {
+	public Valuta pronadjiValutu(Valuta valuta) {
+		if (valuteMenjacnice.isEmpty())
+			throw new RuntimeException();
+
+		for (int i = 0; i < valuteMenjacnice.size(); i++)
+			if (valuteMenjacnice.get(i).equals(valuta))
+				return valuteMenjacnice.get(i);
+
 		return null;
 	}
-	
-	
 
 }
