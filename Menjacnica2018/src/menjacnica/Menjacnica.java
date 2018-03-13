89614ab6e000201923dfc7786a6ec18db963c958
urkeev14 <uros.veljkovic1996@gmail.com>
2018-03-13 22:23:05 +0100

    Kreirana klasa Menjacnica bez implementiranih metoda

---
+++ Menjacnica2018/src/menjacnica/Menjacnica.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import interfjes.InterfejsMenjacnice;
+import valuta.Valuta;
+
+public class Menjacnica implements InterfejsMenjacnice{
+
+	@Override
+	public void dodajKurs(Valuta valuta, GregorianCalendar dan) {
+		
+	}
+
+	@Override
+	public void obrisiKurs(Valuta valuta, GregorianCalendar dan) {
+		
+	}
+
+	@Override
+	public Valuta pronadjiValutu(Valuta valuta, GregorianCalendar dan) {
+		return null;
+	}
+	
+	
+
+}
