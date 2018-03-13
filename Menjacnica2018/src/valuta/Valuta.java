3e6b4f5ac2b7cab913c25399e9a7adc0efd9e4da
urkeev14 <uros.veljkovic1996@gmail.com>
2018-03-13 22:19:30 +0100

    Uvedena logicka kontrola za klasu Valuta

--- Menjacnica2018/src/valuta/Valuta.java
+++ Menjacnica2018/src/valuta/Valuta.java
@@ -2,6 +2,8 @@ package valuta;
 
 import java.util.GregorianCalendar;
 
+import javax.management.RuntimeErrorException;
+
 public class Valuta {
 	
 	private String naziv;
@@ -15,36 +17,48 @@ public class Valuta {
 		return naziv;
 	}
 	public void setNaziv(String naziv) {
+		if(naziv == null || naziv.isEmpty())
+			throw new RuntimeException("Greska pri unosenju naziva valute");
 		this.naziv = naziv;
 	}
 	public String getSkraceniNaziv() {
 		return skraceniNaziv;
 	}
 	public void setSkraceniNaziv(String skraceniNaziv) {
+		if(skraceniNaziv == null || skraceniNaziv.isEmpty())
+			throw new RuntimeException("Greska pri unosenju naziva valute");
 		this.skraceniNaziv = skraceniNaziv;
 	}
 	public double getSrednjiKurs() {
 		return srednjiKurs;
 	}
 	public void setSrednjiKurs(double srednjiKurs) {
+		if(srednjiKurs < 0)
+			throw new RuntimeException("Greska pri unosenju srednjeg kursa");
 		this.srednjiKurs = srednjiKurs;
 	}
 	public double getKupovniKurs() {
 		return kupovniKurs;
 	}
 	public void setKupovniKurs(double kupovniKurs) {
+		if(kupovniKurs < 0)
+			throw new RuntimeException("Greska pri unosenju kupovnog kursa");
 		this.kupovniKurs = kupovniKurs;
 	}
 	public double getProdajniKurs() {
 		return prodajniKurs;
 	}
 	public void setProdajniKurs(double prodajniKurs) {
+		if(prodajniKurs < 0)
+			throw new RuntimeException("Greska pri unosenju prodajnog kursa");
 		this.prodajniKurs = prodajniKurs;
 	}
 	public GregorianCalendar getDatum() {
 		return datum;
 	}
 	public void setDatum(GregorianCalendar datum) {
+		if(datum == null || datum.before(new GregorianCalendar()))
+			throw new RuntimeException("Greska pri unosenju datuma");
 		this.datum = datum;
 	}
 
