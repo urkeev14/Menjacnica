c04ed2d109d7fa792e8cf6da61ab67d2b630d457
urkeev14 <uros.veljkovic1996@gmail.com>
2018-03-13 22:04:41 +0100

    Kreirana klasa Valuta

---
+++ Menjacnica2018/src/valuta/Valuta.java
@@ -0,0 +1,53 @@
+package valuta;
+
+import java.util.GregorianCalendar;
+
+public class Valuta {
+	
+	private String naziv;
+	private String skraceniNaziv;
+	private double srednjiKurs;
+	private double kupovniKurs;
+	private double prodajniKurs;
+	private GregorianCalendar datum;
+	
+	public String getNaziv() {
+		return naziv;
+	}
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+	public String getSkraceniNaziv() {
+		return skraceniNaziv;
+	}
+	public void setSkraceniNaziv(String skraceniNaziv) {
+		this.skraceniNaziv = skraceniNaziv;
+	}
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+	public GregorianCalendar getDatum() {
+		return datum;
+	}
+	public void setDatum(GregorianCalendar datum) {
+		this.datum = datum;
+	}
+	
+	
+
+}
